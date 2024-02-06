ad89b84457a8180f22777f0547c75adcaad0d3b4
Akbarshox <akbarshox.anwarjonow@gmail.com>
2024-02-06 23:45:51 +0500

    legend

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let days = document.querySelectorAll(".timer_value")[0]
 let hours = document.querySelectorAll(".timer_value")[1]
 let minutes = document.querySelectorAll(".timer_value")[2]
 let second = document.querySelectorAll(".timer_value")[3]
-let deadlineDate = new Date(2024 , 1 , 2 , 23 , 59 , 59)
+let deadlineDate = new Date(2024 , 2 , 2 , 23 , 59 , 59)
 
 function countDown(){
     let newDate = new Date()
